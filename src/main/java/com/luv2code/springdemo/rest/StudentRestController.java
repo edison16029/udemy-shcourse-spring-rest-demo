89c84eb6738df0a77e0cadfb86824b4e0b46586e
Edison <edison16029@gmail.com>
2021-12-07 19:44:15 +0530

    Getting student by student id (or index)

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +16,28 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		
-		List<Student> theStudents = new ArrayList<Student>();
+	private List<Student> theStudents;
+	
+	//define @PostConstruct to load the student data, this is called only once, during Bean Initialisation
+	
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<Student>();
 		
 		theStudents.add(new Student("Edison", "Moni"));
 		theStudents.add(new Student("Lionel", "Messi"));
 		theStudents.add(new Student("Lebron", "James"));
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		
 		return theStudents;
+	}
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		return theStudents.get(studentId);
 	}
 }
